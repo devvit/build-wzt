de609d494557d6ff01d7f77f676fe6c56e83c34c
devvit <1715224+devvit@users.noreply.github.com>
2024-07-20 13:28:13 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -20,4 +20,4 @@ for f in $(ls *.ttf *.ttc); do
     perl -pi.bak -e "s/font\!\(\"..\/..\/assets\/fonts\/SymbolsNerdFontMono-Regular.ttf\"\)/font\!\(\"..\/..\/assets\/fonts\/SymbolsNerdFontMono-Regular.ttf\"\),font\!\(\"..\/..\/assets\/fonts\/$f\"\)/g" wezterm-font/src/parser.rs
 done
 
-perl -pi.bak -e 's/elem.max_width =/elem.max_width = Some\(Dimension::Pixels\(self.dimensions.pixel_width as f32 \/ num_tabs\)\); elem.min_width = Some\(Dimension::Pixels\(self.dimensions.pixel_width as f32 \/ num_tabs\)\);\/\//g' wezterm-gui/src/termwindow/render/fancy_tab_bar.rs
+perl -pi.bak -e 's/elem.max_width =/elem.max_width = Some\(Dimension::Pixels\(self.dimensions.pixel_width as f32 \/ num_tabs\)\); elem.min_width = elem.max_width;\/\//g' wezterm-gui/src/termwindow/render/fancy_tab_bar.rs
